e7f5506bd9adb3536638f269f53ea7b47a0cfa26
Rob Thomas <robt84@gmail.com>
2022-12-22 13:00:22 -0600

    added the stripe to the header left section

--- acf-blocks/header-img-left.php
+++ acf-blocks/header-img-left.php
@@ -1,5 +1,6 @@
 <section class="header-img-left pb-5">
-    <div class="container-fluid">
+    <img class="stripe-right" src="<?php echo get_stylesheet_directory_uri(); ?>/img/stripe-right.svg" alt="a red line">
+    <div class="container-fluid section-index">
         <div class="row d-flex align-items-center">
             <div class="col-sm-12 col-md-12 col-lg-6 text-center mb-5 mb-sm-5 mb-md-5 mb-lg-0">
                 <?php $image = get_sub_field( 'image' ); ?>
