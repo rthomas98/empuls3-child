cad5bb95d1ea2abb28e7e29c04a7bffe176dc564
Rob Thomas <robt84@gmail.com>
2023-01-12 18:09:38 -0600

    worked on the blog template

--- index.php
+++ index.php
@@ -19,15 +19,71 @@ get_header();
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
-<section class="py-5 blog-banner d-flex align-items-center">
-    <div class="container">
-        <div class="row">
-            <h1 class="display-3">Insights</h1>
+<section class="py-5 blog-banner d-flex align-items-center position-relative">
+    <img class="stripe-right" src="<?php echo get_stylesheet_directory_uri(); ?>/img/stripe-right.svg" alt="a red line">
+    <div class="container section-index">
+        <div class="row d-flex align-items-center">
+            <div class="col-sm-12 col-md-12 col-lg-6">
+                <p class="lead">
+                    Providing IT professionals and agency owners with insights into the intersection of technology and business.
+                </p>
+                <h1 class="display-3">Empuls3 Insights</h1>
+            </div>
+            <div class="col-sm-12 col-md-12 col-lg-6"></div>
         </div>
     </div>
 </section>
 
-<div class="container py-5">
+    <!-- Featured blog post section -->
+    <section class="featured-post py-5">
+        <div class="container">
+            <div class="row mb-4">
+                <div class="col">
+                    <h2 class="display-4">Featured Post</h2>
+                </div>
+            </div>
+            <?php
+            $args = array(
+                'meta_key' => 'featured_post',
+                'meta_value' => '1',
+                'posts_per_page' => 1
+            );
+            $featured_query = new WP_Query( $args );
+            while ( $featured_query->have_posts() ) : $featured_query->the_post(); ?>
+            <div class="row d-flex align-items-center">
+                <div class="col-sm-12 col-md-12 col-lg-6">
+                    <div class="featured-post-image">
+                        <?php the_post_thumbnail('large', array( 'class' => 'img-fluid' )); ?>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-12 col-lg-6">
+                    <div class="featured-post-date">
+                        <p><?php the_date(); ?></p>
+                    </div>
+                    <h3 class="display-6"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+                    <div class="featured-post-author">
+                        <?php echo get_avatar(get_the_author_meta('ID'), 32); ?>
+                        <p><?php the_author(); ?></p>
+                    </div>
+                    <div class="featured-post-excerpt">
+                        <?php the_excerpt(); ?>
+                    </div>
+                </div>
+
+            </div>
+            <?php endwhile; wp_reset_postdata(); ?>
+
+            <hr class="mt-5">
+        </div>
+    </section>
+
+
+    <div class="container py-5">
+        <div class="row mb-4">
+            <div class="col">
+                <h2 class="display-4">Most Recent post</h2>
+            </div>
+        </div>
     <div class="row">
     <?php
     // The Query
